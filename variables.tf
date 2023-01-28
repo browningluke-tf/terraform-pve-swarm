ea17fefe18322f4b159cec04959281b2aaf188be
Luke Browning <browningluke01@gmail.com>
2023-01-27 19:10:05 -0800

    Remove index var from worker map

--- variables.tf
+++ variables.tf
@@ -32,7 +32,6 @@ variable "managers" {
 
 variable "workers" {
   type = map(object({
-    index     = number
     cores     = optional(number, 1)
     memory    = optional(number, 2048)
     balloon   = optional(number, 512)
