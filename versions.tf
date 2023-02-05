c400c840d68c0d19b7afaf7ef139fb208deb9a1d
Luke Browning <browningluke01@gmail.com>
2023-02-04 19:11:33 -0800

    Lint TF code with tflint

--- versions.tf
+++ versions.tf
@@ -1,7 +1,10 @@
 terraform {
+  required_version = "~> 1.3.7"
+
   required_providers {
     doppler = {
-      source = "DopplerHQ/doppler"
+      source  = "DopplerHQ/doppler"
+      version = "~> 1.1.6"
     }
     proxmox = {
       source  = "telmate/proxmox"
